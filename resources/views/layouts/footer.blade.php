3f910da4679f57ee2553a5a54c8f4258734f20e2
Himaniahire <himaniahiretechomax@gmail.com>
2025-04-28 16:09:49 +0530

    Laravel

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,93 @@
+<footer class="site-footer">
+    <div class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget about-widget">
+                        <div class="widget-title">
+                            <h3>About Us</h3>
+                        </div><!-- /.widget-title -->
+                        <p>We are the leaders in the building industries and factories. We’re wordl wide. We
+                            never give up on the challenges.</p>
+                        <img src="images/footer-map-1-1.jpg" alt="Awesome Image" />
+                        <a href="#" class="find-us-link"><i class="fa fa-map-marker"></i>Find us on Map</a>
+                    </div><!-- /.footer-widget -->
+                </div><!-- /.col-lg-3 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget links-widget">
+                        <div class="widget-title">
+                            <h3>Links</h3>
+                        </div><!-- /.widget-title -->
+                        <div class="link-wrapper clearfix">
+                            <ul class="link-lists">
+                                <li><a href="#">Why Choose</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Case Studies</a></li>
+                                <li><a href="#">Our Services</a></li>
+                                <li><a href="#">Sitemap</a></li>
+                            </ul>
+                            <ul class="link-lists">
+                                <li><a href="#">Price Guide</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                            </ul><!-- /.link-lists -->
+                        </div><!-- /.link-wrapper -->
+                    </div><!-- /.footer-widget -->
+                </div><!-- /.col-lg-3 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget service-links-widget">
+                        <div class="widget-title">
+                            <h3>Services</h3>
+                        </div><!-- /.widget-title -->
+                        <ul class="link-lists">
+                            <li><a href="#">Petroleum and Gas</a></li>
+                            <li><a href="#">Industrial Construction</a></li>
+                            <li><a href="#">Chemical Research</a></li>
+                            <li><a href="#">Mechanical Engineering</a></li>
+                            <li><a href="#">Power Energies</a></li>
+                        </ul><!-- /.link-lists -->
+                    </div><!-- /.footer-widget -->
+                </div><!-- /.col-lg-3 -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget contact-widget">
+                        <ul class="contact-infos">
+                            <li>
+                                <i class="facdori-icon-placeholder-2"></i>
+                                66 Road Broklyn Street, 600 <br> New York, USA
+                            </li>
+                            <li>
+                                <i class="facdori-icon-message"></i>
+                                needhelp@facdori.com
+                            </li>
+                            <li>
+                                <i class="facdori-icon-phone-call-1"></i>
+                                666 888 0000
+                            </li>
+                        </ul><!-- /.contact-infos -->
+                    </div><!-- /.footer-widget -->
+                </div><!-- /.col-lg-3 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.main-footer -->
+    <div class="bottom-footer">
+        <div class="container ">
+            <div class="container-inner clearfix">
+                <div class="left-content float-left">
+                    <p>&copy; Copyright 2019 by TemplatePath.net</p>
+                </div><!-- /.left-content float-left -->
+                <div class="right-content float-right">
+                    <div class="social">
+                        <a href="#"><i class="fa fa-facebook-f"></i></a>
+                        <!--
+                    --><a href="#"><i class="fa fa-twitter"></i></a>
+                        <!--
+                    --><a href="#"><i class="fa fa-linkedin"></i></a>
+                        <!--
+                    --><a href="#"><i class="fa fa-youtube-play"></i></a>
+                    </div><!-- /.social -->
+                </div><!-- /.left-content float-left -->
+            </div><!-- /.container-inner -->
+        </div><!-- /.container -->
+    </div><!-- /.bottom-footer -->
+</footer><!-- /.site-footer -->
---
+++ resources/views/layouts/footer_script.blade.php
@@ -0,0 +1,21 @@
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/theme.js"></script>
+
+
+
+
+
+</body>
+
+
+</html>
