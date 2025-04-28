5c818ec3c4677cbb57dd92ad1c8e8c4aad8eb7c7
nikeshjaintms <nikeshjaintms@gmail.com>
2025-04-28 17:45:33 +0530

    UnWanted remove

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,127 @@
+<header class="site-header header-two">
+    <div class="top-bar">
+        <div class="container clearfix">
+            <div class="left-content float-left">
+                <div class="single-header-contact-info">
+                    <div class="icon-block">
+                        <i class="facdori-icon-message"></i>
+                    </div><!-- /.icon-block -->
+                    <div class="text-block">
+                        <p>needhelp@facdori.com</p>
+                    </div><!-- /.text-block -->
+                </div><!-- /.single-header-contact-info -->
+                <div class="single-header-contact-info">
+                    <div class="icon-block">
+                        <i class="facdori-icon-phone-call-1"></i>
+                    </div><!-- /.icon-block -->
+                    <div class="text-block">
+                        <p>666 888 0000</p>
+                    </div><!-- /.text-block -->
+                </div><!-- /.single-header-contact-info -->
+                <div class="single-header-contact-info">
+                    <div class="icon-block">
+                        <i class="facdori-icon-clock1"></i>
+                    </div><!-- /.icon-block -->
+                    <div class="text-block">
+                        <p>Mon to Fri 9:00am to 6:00pm</p>
+                    </div><!-- /.text-block -->
+                </div><!-- /.single-header-contact-info -->
+                <div class="single-header-contact-info">
+                    <div class="icon-block">
+                        <i class="facdori-icon-placeholder-2"></i>
+                    </div><!-- /.icon-block -->
+                    <div class="text-block">
+                        <p>66 Broklyn Street USA</p>
+                    </div><!-- /.text-block -->
+                </div><!-- /.single-header-contact-info -->
+            </div><!-- /.left-content -->
+            <div class="right-content float-right">
+                <select class="selectpicker" data-width="fit">
+                    <option data-content='<span class="flag-icon flag-icon-gb flag-icon-squared"></span> Eng'>
+                        Eng</option>
+                    <option data-content='<span class="flag-icon flag-icon-bd flag-icon-squared"></span> Ban'>
+                        Ban</option>
+                    <option data-content='<span class="flag-icon flag-icon-in flag-icon-squared"></span> Ind'>
+                        Ind</option>
+                </select>
+            </div><!-- /.right-content -->
+        </div><!-- /.container -->
+    </div><!-- /.top-bar -->
+    <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+        <div class="container clearfix">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="logo-box clearfix">
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/logo-1-1.png" alt="Awesome Image" />
+                </a>
+
+                <button class="menu-toggler" data-target="#main-nav-bar">
+                    <span class="fa fa-bars"></span>
+                </button>
+            </div><!-- /.logo-box -->
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="main-navigation" id="main-nav-bar">
+                <ul class=" navigation-box">
+                    <li class=" current">
+                        <a href="index.html">Home</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Home One</a></li>
+                            <li><a href="index2.html">Home Two</a></li>
+                            <li><a href="index3.html">Home Three</a></li>
+                            <li><a href="index4.html">Home Four</a></li>
+                            <li><a href="index4.html">Header Versions</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index.html">Header One</a></li>
+                                    <li><a href="index2.html">Header Two</a></li>
+                                    <li><a href="index3.html">Header Three</a></li>
+                                    <li><a href="index4.html">Header Four</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul><!-- /.sub-menu -->
+                    </li>
+                    <li>
+                        <a href="#">Pages</a>
+                        <ul class="sub-menu">
+                            <li><a href="about.html">About Us 01 </a></li>
+                            <li><a href="about2.html">About Us 02</a></li>
+                            <li><a href="whychoose.html">Why Choose</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li><a href="history.html">History</a></li>
+                        </ul><!-- /.sub-menu -->
+                    </li>
+                    <li>
+                        <a href="services.html">Our Services</a>
+                        <ul class="sub-menu">
+                            <li><a href="service.html">Services 01</a></li>
+                            <li><a href="service2.html">Services 02</a></li>
+                            <li><a href="service3.html">Services 03</a></li>
+                            <li><a href="service-details.html">Service Details</a></li>
+                        </ul><!-- /.sub-menu -->
+                    </li>
+                    <li>
+                        <a href="project.html">Recent Projects</a>
+                        <ul class="sub-menu">
+                            <li><a href="project.html">Projects 01</a></li>
+                            <li><a href="project2.html">Project 02</a></li>
+                            <li><a href="project-details.html">Project Details</a></li>
+                        </ul><!-- /.sub-menu -->
+                    </li>
+                    <li>
+                        <a href="news.html">News</a>
+                        <ul class="sub-menu">
+                            <li><a href="news.html">News Grid</a></li>
+                            <li><a href="news-2col.html">News 2 Column</a></li>
+                            <li><a href="news-sidebar.html">News Right Sidebar</a></li>
+                            <li><a href="single-news.html">News Details</a></li>
+                        </ul><!-- /.sub-menu -->
+                    </li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+</header><!-- /.site-header -->
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>JKEE Group</title>
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:300,400,500,600,700,800,900" rel="stylesheet">
+
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/hover-min.css">
+    <link rel="stylesheet" type="text/css" href="plugins/facdori-icon/style.css">
+    <link rel="stylesheet" type="text/css" href="plugins/bands-icon/style.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="plugins/flag-icon-css/css/flag-icon.min.css">
+
+
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+
+    <div class="page-wrapper">
+
+        <div class="preloader"></div>
+
+        @include('layouts.header')
+        @yield('content')
+        @include('layouts.footer')
+
+
+    </div><!-- /.page-wrapper -->
+
+
+
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- /.scroll-to-top -->
+    @include('layouts.footer_script')
